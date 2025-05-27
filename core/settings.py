5bef71168c562877505235d9ddefdb55749e1fd3
jhonRR28 <rmales1728@hotmail.com>
2025-05-27 14:57:11 -0500

    npm

--- core/settings.py
+++ core/settings.py
@@ -49,8 +49,8 @@ INSTALLED_APPS = [
 
 TAILWIND_APP_NAME = 'theme'
 
-NPM_BIN_PATH = "C:/Program Files/nodejs/npm.cmd"
-# NPM_BIN_PATH = "/usr/bin/npm"
+# NPM_BIN_PATH = "C:/Program Files/nodejs/npm.cmd"
+NPM_BIN_PATH = "/usr/bin/npm"
 
 
 MIDDLEWARE = [

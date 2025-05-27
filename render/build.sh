915c7193b496b01a92d1478f5d82477354f1263b
jhonRR28 <rmales1728@hotmail.com>
2025-05-27 15:08:31 -0500

    buid render

--- render/build.sh
+++ render/build.sh
@@ -1,9 +1,20 @@
 set -o errexit
 
+# Instalar dependencias de Python
 pip install -r requirements.txt
 
+# Recoger archivos estáticos
 python manage.py collectstatic --no-input
 
+# Aplicar migraciones
 python manage.py migrate
 
+# Instalar dependencias de npm para Tailwind
+cd theme/static_src
+npm install
+
+# Volver a raíz del proyecto
+cd ../../
+
+# Ejecutar build de Tailwind (ahora rimraf ya existe)
 python manage.py tailwind build
